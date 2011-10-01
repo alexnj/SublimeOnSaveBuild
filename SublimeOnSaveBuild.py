1c4f17c17d411ad61d3c56d2b9a9512c11b99dd2
Alex N J <alexnj@gmail.com>
2011-10-01 17:19:24 +0530

    Reverted back the old underscore based API. The camel case one doesn't seem to work reliably. Not sure where I picked it up from.

--- SublimeOnSaveBuild.py
+++ SublimeOnSaveBuild.py
@@ -1,6 +1,5 @@
 import sublime, sublime_plugin
 
 class SublimeOnSaveBuild( sublime_plugin.EventListener ):
-    def onPostSave( self, view ):
-        if view.window().canRunCommand( "build" ):
-            view.window().run_command( "build" )
+    def on_post_save( self, view ):
+        view.window().run_command( "build" )
